3e3210d306eaaa913809ed93ab2ba4eb83641b4b
Tim van Oosterhout <Tim.vanOosterhout@gmail.com>
2026-03-29 12:00:14 +0200

    Add submit button

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 
 app = Flask(__name__, static_folder='site', static_url_path='')
 
@@ -6,6 +6,13 @@ app = Flask(__name__, static_folder='site', static_url_path='')
 @app.route('/')
 def index():
     return send_from_directory('site', 'index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    print(f"Received: container {data['container_oc']}, condition: {data['condition']}")
+    return '', 204
 
 
 if __name__ == '__main__':
